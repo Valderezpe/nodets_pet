6f780d36ad062200e4d8243e25355b65df97c10c
jose Valderez <valderezpe@gmail.com>
2022-06-28 13:37:24 -0300

    Criando controller, models,routes

--- src/controller/pageController.ts
+++ src/controller/pageController.ts
@@ -1,9 +1,40 @@
 import { Request, Response } from "express";
+import { createMenuObject } from '../helpers/createMenuObject';
 
 export const home = (req: Request, res: Response) => {
-    //res.render('pages/page');
+    res.render('pages/page', {
+        menu:createMenuObject(''),
+        banner: {
+            title: 'Todos os animais',
+            background: 'allanimals.jpg'
+        }
+    });
 }
 
-export const dogs = (req: Request, res: Response) => {}
-export const cats = (req: Request, res: Response) => {}
-export const fishes= (req: Request, res: Response) => {}
+export const dogs = (req: Request, res: Response) => {
+    res.render('pages/page', {
+        menu:createMenuObject('dog'),
+        banner: {
+            title: '',
+            background: 'banner_dog.jpg'
+        }
+    });
+}
+export const cats = (req: Request, res: Response) => {
+    res.render('pages/page', {
+        menu:createMenuObject('cat'),
+        banner: {
+            title: '',
+            background: 'banner_cats.jpg'
+        }
+    });
+}
+export const fishes= (req: Request, res: Response) => {
+    res.render('pages/page', {
+        menu:createMenuObject('fish'),
+        banner: {
+            title: '',
+            background: 'banner_fish.jpg'
+        }
+    });
+}
